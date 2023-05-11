2fb9e0cc0f0f84243606fc9185a2d9b589a80879
DESKTOP-SBGC454\eduar <em961226@gmail.com>
2023-05-11 20:12:43 -0300

    exercicio1

---
+++ Exercicios_Oracle/src/exercicio1.java
@@ -0,0 +1,38 @@
+public class exercicio1 {
+    public static void main(String[] args) throws Exception {
+ 
+
+      /*   System.out.println( "  x x x x   ");
+        System.out.println( " x       x  ");
+        System.out.println( "x x     x x ");
+        System.out.println( "x         x ");
+        System.out.println( "x  x   x  x ");
+        System.out.println( "x   xxx   x ");
+        System.out.println( " x       x   ");
+        System.out.println( "  x x x x   ");
+       */
+
+      /*    System.out.println( "     /|        /|     ");
+        System.out.println( "    / |_______/ |  ");
+        System.out.println( "   /            |  ");
+        System.out.println( "  ( (o)    (o)  )  ");
+        System.out.println( " ====   Y    ====   ");
+        System.out.println( " ==== (_|_) ====      ");
+        System.out.println( "   (           )     ");
+        System.out.println( "     (_______)      ");
+*/
+
+/*Pedido de Caixa
+-Propriedades: Tamanho, Design Personalizado, Cliente
+-Comportamentos: Adicionar ao carrinho, Remover do carrinho, Salvar pedido
+*Conta do Cliente
+-Propriedades: Nome do Cliente, Endereço, Histórico de Pedidos
+=Comportamentos: Criar conta, Fazer login, Verificar histórico de pedidos
+*Processamento de Pedidos
+-Propriedades: Número do Pedido, Estado do Pedido, Data de Entrega
+-Comportamentos: Verificar estado do pedido, Atualizar estado do pedido */
+
+
+
+    }
+}
